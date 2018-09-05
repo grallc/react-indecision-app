9f761bc51c97266bf75b36b3c3ea73cc22081fec
Corentin Grall <corentin.grall@gmail.com>
2018-09-05 19:38:05 +0200

    Counter example is now using React Components

--- src/playground/build-it-visibility.js
+++ src/playground/build-it-visibility.js
@@ -1,29 +1,36 @@
-let visibility = false;
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: true
+        }
+    }
 
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        });
+    }
 
-const toggleVisibility = () => {
-    visibility = !visibility;
-    render();
+    render(){
+        return (
+     <div>
+         <h1>Visibility Toggle</h1>
+         <button onClick={this.handleToggleVisibility}>
+             {this.state.visibility ? 'Hide details' : 'Show details'}
+         </button>
+         {this.state.visibility && (
+             <div>
+                 <p>Hey !</p>
+             </div>
+         )}
+     </div>
+        );
+    }
 }
 
-const appRoot = document.getElementById('app');
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
-const render = () => {
-  const template = (
-    <div>
-        <h1>Visibility Toggle</h1>
-        <button onClick={toggleVisibility}>
-            {visibility ? 'Hide details' : 'Show details'}
-        </button>
-        {visibility && (
-            <div>
-                <p>Hey !</p>
-            </div>
-        )}
-    </div>
-  );
-
-  ReactDOM.render(template, appRoot);
-};
-
-render();
