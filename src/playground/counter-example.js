2eac3770e5533b8a1255de10d9fec7d8369b92ad
Corentin Grall <corentin.grall@gmail.com>
2018-09-05 11:57:32 +0200

    Started recreating Counter example using Components :arrows_counterclockwise:

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,29 +1,32 @@
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
+class Counter extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+
+    }
+    handleAddOne() {
+        console.log('add');
+    }
+    handleMinusOne() {
+        console.log('remove');
+    }
+    handleReset() {
+        console.log('reset');
+    }
+    render(){
+        return (
+            <div>
+                <h1>Count: 5</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>Reset</button>
+            </div>
+        );
+    }
 }
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-}
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-}
 
-const renderCounterApp = () => {
-    const templateThree = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
-        </div>
-    );
 
-    ReactDOM.render(templateThree, appRoot);
 
-};
-
-renderCounterApp();
+ReactDOM.render(<Counter />, document.getElementById('app'));
