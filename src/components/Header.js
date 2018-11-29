7c652cd15baaaadcb8ae2068e9e5e5d116f5614c
Corentin Grall <corentin.grall@gmail.com>
2018-11-29 16:51:00 +0100

    Started designing the application :construction:

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = (props) => (
-  <div>
-    <h1>{props.title}</h1>
-    {props.subtitle && <h2>{props.subtitle}</h2>}
+  <div className="header">
+    <h1 className="header__title">{props.title}</h1>
+    {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
   </div>
 );
-  Header.defaultProps = {
-    title: 'Indecision'
-  };
+Header.defaultProps = {
+  title: 'Indecision'
+};
 
-  export default Header;
+export default Header;
