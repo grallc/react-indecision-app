40a6b665aecf9bfb53b3bccba397a350ec3c6955
Corentin Grall <corentin.grall@gmail.com>
2018-11-30 18:55:19 +0100

    Finished design ! :lipstick:

--- src/components/Action.js
+++ src/components/Action.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 const Action = (props) => (
   <div>
     <button
+      className="big-button"
       onClick={props.handlePick}
       disabled={!props.hasOptions}
     >
--- src/components/Option.js
+++ src/components/Option.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 
 
 export default Option = (props) => (
-  <div>
-    {props.optionText}
+  <div className="option">
+    <p className="option__text">{props.count}. {props.optionText}</p>
     <button
+      className="button button--link"
       onClick={(e) => {
         props.handleDeleteOption(props.optionText);
       }}
     >
       remove
-    </button>
+      </button>
   </div>
 );
